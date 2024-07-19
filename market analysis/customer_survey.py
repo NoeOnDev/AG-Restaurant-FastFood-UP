8705a96aabb40ce28136b34fa096ce5cb2df0289
NoeOnDev <alxg5516@gmail.com>
2024-07-18 23:50:27 -0600

    feat: Generate customer survey data and save as CSV file
    This commit adds code to generate customer survey data and saves it as a CSV file in the 'market analysis' directory. The generated data includes information about the customer's favorite drink, favorite food, favorite dessert, and frequency of consumption. This data will be used for market analysis and customer behavior studies.

---
+++ market analysis/customer_survey.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import random
+
+bebidas = ["Pozol", "Coca-Cola"]
+comidas = ["Gordita", "Empanada", "Taco"]
+postres = ["Nuegado", "Turrón"]
+frecuencia_consumo = ["Diario", "Semanal", "Mensual", "Rara vez"]
+
+num_encuestas = 1000
+
+encuestas = []
+for i in range(1, num_encuestas + 1):
+    bebida = random.choice(bebidas)
+    comida = random.choice(comidas)
+    postre = random.choice(postres)
+    frecuencia = random.choice(frecuencia_consumo)
+    
+    encuestas.append([
+        i,
+        bebida,
+        comida,
+        postre,
+        frecuencia
+    ])
+
+df_encuestas = pd.DataFrame(encuestas, columns=[
+    "Número de Encuesta",
+    "Bebida Favorita",
+    "Comida Favorita",
+    "Postre Favorito",
+    "Frecuencia de Consumo"
+])
+
+df_encuestas.to_csv('market analysis/encuestas_clientes.csv', index=False)
+
+df_encuestas.head()
