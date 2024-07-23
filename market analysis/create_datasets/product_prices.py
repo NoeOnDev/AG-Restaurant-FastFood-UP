d068332e1ee1369164bb928cc9b8197cd98a3bbf
NoeOnDev <alxg5516@gmail.com>
2024-07-23 12:31:52 -0600

    Actualizar precios de productos en el archivo precios_productos.csv

--- market analysis/create_datasets/product_prices.py
+++ market analysis/create_datasets/product_prices.py
@@ -1,26 +1,32 @@
 import pandas as pd
 
 productos_dict = {
-    "Pozol": {"costo": 7, "venta": 15},
-    "Coca-Cola": {"costo": 18, "venta": 25},
-    "Tascalate": {"costo": 8, "venta": 14},
-    "Agua de chía": {"costo": 11, "venta": 25},
-    "Agua de horchata": {"costo": 10, "venta": 20},
-    "Agua de jamaica": {"costo": 9, "venta": 18},
-    "Quesadilla": {"costo": 12, "venta": 22},
-    "Gordita": {"costo": 10, "venta": 18},
-    "Taco": {"costo": 9, "venta": 14},
-    "Empanada": {"costo": 8, "venta": 15},
-    "Tamal": {"costo": 10, "venta": 20},
-    "Tostada": {"costo": 7, "venta": 12},
-    "Turrón": {"costo": 4, "venta": 10},
-    "Nuegado": {"costo": 5, "venta": 12},
-    "Turulete": {"costo": 3, "venta": 10},
-    "Cocada": {"costo": 4, "venta": 8},
-    "Plátano Asado": {"costo": 4, "venta": 9},
-    "Bunuelo": {"costo": 5, "venta": 10},
+    "Pozol": {"costo": 7, "venta": 16},
+    "Coca-Cola": {"costo": 18, "venta": 26},
+    "Tascalate": {"costo": 8, "venta": 15},
+    "Agua de chía": {"costo": 11, "venta": 24},
+    "Agua de horchata": {"costo": 10, "venta": 21},
+    "Agua de jamaica": {"costo": 9, "venta": 19},
+    "Quesadilla": {"costo": 12, "venta": 23},
+    "Gordita": {"costo": 10, "venta": 19},
+    "Taco": {"costo": 9, "venta": 15},
+    "Empanada": {"costo": 8, "venta": 16},
+    "Tamal": {"costo": 10, "venta": 21},
+    "Tostada": {"costo": 7, "venta": 13},
+    "Turrón": {"costo": 4, "venta": 11},
+    "Nuegado": {"costo": 5, "venta": 13},
+    "Turulete": {"costo": 3, "venta": 9},
+    "Cocada": {"costo": 4, "venta": 9},
+    "Plátano Asado": {"costo": 4, "venta": 10},
+    "Bunuelo": {"costo": 5, "venta": 11},
 }
 
 productos_df = pd.DataFrame.from_dict(productos_dict, orient='index')
 
-productos_df.to_csv('market analysis/datasets/precios_productos.csv')
+productos_df['producto'] = productos_df.index
+
+productos_df = productos_df[['producto', 'costo', 'venta']]
+
+productos_df = productos_df.reset_index(drop=True)
+
+productos_df.to_csv('market analysis/datasets/precios_productos.csv', index=False)
